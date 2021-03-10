febc83336f911773914caef23365285d7d54dfb7
dpoldrugo <davor.poldrugo@infobip.com>
2021-03-10 10:32:39 +0100

    fix build failing on heroku - make sure to use correct custom fields in test, even when they are overriden in ENV (it was problem with CUSTOM_FIELD_INTEGRATION_METADATA) v2

--- src/service/processor/potres2020_to_potres.app/Config.ts
+++ src/service/processor/potres2020_to_potres.app/Config.ts
@@ -1,19 +1,26 @@
 import {EntryStatus} from "./PotresAppModel";
-export const POTRES_APP_BACKEND_BASE_URL = process.env.POTRES_APP_BACKEND_BASE_URL || 'http://localhost:1337';
-export const POTRES_APP_API_KEY = process.env.POTRES_APP_API_KEY || 'davor-dev-token-fdsfdsfldjfddkgfgdf';
+const dotenv = require('dotenv');
+const fs = require('fs');
 
-export const POTRES2020_USERNAME = process.env.POTRES2020_USERNAME;
-export const POTRES2020_PASSWORD = process.env.POTRES2020_PASSWORD;
-export const POTRES2020_CLIENT_ID = process.env.POTRES2020_CLIENT_ID;
-export const POTRES2020_CLIENT_SECRET = process.env.POTRES2020_CLIENT_SECRET;
+let config: any = process.env;
+if (process.env.NODE_ENV && process.env.NODE_ENV.includes('test')) {
+    config = dotenv.parse(fs.readFileSync(`./.env.${process.env.NODE_ENV}`));
+}
+export const POTRES_APP_BACKEND_BASE_URL = config.POTRES_APP_BACKEND_BASE_URL || 'http://localhost:1337';
+export const POTRES_APP_API_KEY = config.POTRES_APP_API_KEY || 'davor-dev-token-fdsfdsfldjfddkgfgdf';
 
-export const CUSTOM_FIELD_INTEGRATION_METADATA = process.env.CUSTOM_FIELD_INTEGRATION_METADATA || '35560aa7-b4a4-4ac5-a5ef-0d280b21b48b'; // integration_metadata
-export const CUSTOM_FIELD_LOCATION = process.env.CUSTOM_FIELD_LOCATION || '87e86d94-df5d-4eb5-9080-5a4d44a5f769'; // lokacija
-export const CUSTOM_FIELD_CONTACT = process.env.CUSTOM_FIELD_CONTACT || '4583d2a1-331a-4da2-86df-3391e152198e'; // kontakt
-export const CUSTOM_FIELD_DETAIL_DESCRIPTION = process.env.CUSTOM_FIELD_DETAIL_DESCRIPTION || '3c8441b3-5744-48bb-9d9e-d6ec4be50613'; // detaljniji opis
-export const CUSTOM_FIELD_NOTES = process.env.CUSTOM_FIELD_NOTES || '6b541a6d-eb02-4824-8662-a741da46b2b3'; // zaključak
+export const POTRES2020_USERNAME = config.POTRES2020_USERNAME;
+export const POTRES2020_PASSWORD = config.POTRES2020_PASSWORD;
+export const POTRES2020_CLIENT_ID = config.POTRES2020_CLIENT_ID;
+export const POTRES2020_CLIENT_SECRET = config.POTRES2020_CLIENT_SECRET;
 
-export const POTRES2020_CHANGES_TO_POTRES_APP_PROCESSOR_MIN_ID_TO_PROCESS = +process.env.POTRES2020_CHANGES_TO_POTRES_APP_PROCESSOR_MIN_ID_TO_PROCESS || 0;
+export const CUSTOM_FIELD_INTEGRATION_METADATA = config.CUSTOM_FIELD_INTEGRATION_METADATA || '35560aa7-b4a4-4ac5-a5ef-0d280b21b48b'; // integration_metadata
+export const CUSTOM_FIELD_LOCATION = config.CUSTOM_FIELD_LOCATION || '87e86d94-df5d-4eb5-9080-5a4d44a5f769'; // lokacija
+export const CUSTOM_FIELD_CONTACT = config.CUSTOM_FIELD_CONTACT || '4583d2a1-331a-4da2-86df-3391e152198e'; // kontakt
+export const CUSTOM_FIELD_DETAIL_DESCRIPTION = config.CUSTOM_FIELD_DETAIL_DESCRIPTION || '3c8441b3-5744-48bb-9d9e-d6ec4be50613'; // detaljniji opis
+export const CUSTOM_FIELD_NOTES = config.CUSTOM_FIELD_NOTES || '6b541a6d-eb02-4824-8662-a741da46b2b3'; // zaključak
+
+export const POTRES2020_CHANGES_TO_POTRES_APP_PROCESSOR_MIN_ID_TO_PROCESS = +config.POTRES2020_CHANGES_TO_POTRES_APP_PROCESSOR_MIN_ID_TO_PROCESS || 0;
 
 enum Potres2020Status {
     published = 'published',
